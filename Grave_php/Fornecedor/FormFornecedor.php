b13a35d4b9174d12f3bd2125e009d7a7b9c2df06
Junkicch <76850261+Junkicch@users.noreply.github.com>
2023-05-08 22:33:48 -0300

    Update FormFornecedor.php

--- Grave_php/Fornecedor/FormFornecedor.php
+++ Grave_php/Fornecedor/FormFornecedor.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <title>Cadastrar Fornecedor</title>
         <link href="../img/Marca D'água Grave Preto.png" rel="icon">
-        <link rel="stylesheet" href="../css/style.css"/>
+        <link rel="stylesheet" href="../css/style-usuario.css"/>
         <!--<link rel="stylesheet" href="../css/formulariotop.css"/>-->
         <script>
             function limpa_formulário_cep() {
